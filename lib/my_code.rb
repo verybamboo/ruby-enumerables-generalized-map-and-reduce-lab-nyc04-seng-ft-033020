67156367477cadabcd91a8a367e4d63c456b38c3
George Chen <georgechen1338@gmail.com>
2020-03-23 17:41:28 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,29 @@
 # Your Code Here
+def map(block)
+  array = []
+  index = 0
+  
+  while index < block.length do
+    array.push(yield(block[index]))
+    index += 1
+  end
+  
+  array
+end
+
+def reduce(block, starting_point = nil)
+  if starting_point  
+    value = starting_point
+    index = 0
+  else
+    value = block[0]
+    index = 1
+  end
+  
+  while index < block.length do
+    value = yield(value, block[index])
+    index += 1
+  end
+  
+  value
+end
